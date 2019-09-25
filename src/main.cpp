27dca7fee88e5f7ffb79397a223a4451f893b8fb
Nestor Huallpa <huallpa.nestor@gmail.com>
2019-09-25 18:50:07 -0300

    Se agrega validaciones

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,20 @@
 int main(int argc, char* argv[]) {
     atexit(FDRegistry::CloseFileDescriptors);
 
-    // TODO: tomar parametros
     t_parametros parametros = Utils::tomarParametros(argc, argv);
 
+    if (parametros.cantDistribuidores > 10) {
+        cout<<"La cantidad de distribuidores debe ser menor de 10"<<endl;
+        return 0;
+    }
+    if (parametros.cantProductores > 10) {
+        cout<<"La cantidad de productore debe ser menor de 10"<<endl;
+        return 0;
+    }
+    if (parametros.cantPuntosVenta > 10) {
+        cout<<"La cantidad de puntos de venta debe ser menor de 10"<<endl;
+        return 0;
+    }
     ProcesoInicial procesoInicial(parametros);
 
     if(parametros.reanudar) {
